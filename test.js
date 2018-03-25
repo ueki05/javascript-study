2d91bad22837ddd00e36ba09b8c4e356b7179828
ueki05 <uekiyuta1991@gmail.com>
2018-03-25 18:43:02 +0900

    10-2-2 DOM要素のプロパティに指定する

--- test.js
+++ test.js
@@ -4,3 +4,10 @@ function stop(event) {
   alert('Stop page transfer');
   return false;
 }
+
+var btn = document.getElementById('foo3');
+function sayFoo3() {
+  alert('foo3');
+}
+
+btn.onclick = sayFoo3;
